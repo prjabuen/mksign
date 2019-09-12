3c023f4ade8aabc74a188c5e3b01d99856f4ecb7
bugszhou <bugszhou@outlook.com>
2019-09-12 18:46:57 +0800

    feat: 对象和数组使用stringify后参与签名

--- src/lib/utils/_getSignStr.js
+++ src/lib/utils/_getSignStr.js
@@ -9,6 +9,9 @@
  * @example  [ ['val1', 2 ], [ 'val2', 5 ] ]
  * @return  String
  */
+import isObject from 'lodash/isObject';
+import isArray from 'lodash/isArray';
+
 function getSignStr(params = [[]]) {
   const noKeys = [],
     strArray = [];
@@ -19,7 +22,7 @@ function getSignStr(params = [[]]) {
     if (key !== 0 && !key) {
       noKeys.push(val);
     } else {
-      strArray.push(`${key}=${val}`);
+      strArray.push((isObject(val) || isArray(val)) ? `${key}=${JSON.stringify(val)}` : `${key}=${val}`);
     }
   });
   return `${strArray.join('&')}${noKeys.join('')}`;
